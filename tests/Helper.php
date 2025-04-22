9088eea06fb104781d886329ba8c29f4a96eb568
chaitanyarai19 <chaitanyarai19@gmail.com>
2025-04-23 00:13:42 +0530

    Revert "added post-commit hook"
    This reverts commit 9f19e20085ebd626f57706b890b18ccd252a6f03.

--- tests/Helper.php
+++ tests/Helper.php
@@ -12,15 +12,3 @@ class Helper
         return $GLOBALS['cache-'.$api->getCurrentApi()][$barCode] ?? $GLOBALS['cache-'.$api->getCurrentApi()][$barCode] = $api->getProduct($barCode);
     }
 }
-
-
-//add 2 numbers
-/*
- * This function adds two numbers together.
- * @param int $a
- * @param int $b
- * @return int
- */
-function add($a, $b) {
-    return $a + $b;
-}
